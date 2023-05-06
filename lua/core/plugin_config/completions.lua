110dd44b7890e910fb82e2aade98b59922e85d98
wil <wilsonsecord@gmail.com>
2023-05-06 19:04:51 -0400

    Added nvim_lsp to the list of completion sources

--- lua/core/plugin_config/completions.lua
+++ lua/core/plugin_config/completions.lua
@@ -5,12 +5,20 @@ cmp.setup({
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-o>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping {
+        i = cmp.mapping.abort(),
+        c = cmp.mapping.close(),
+    },
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
   }),
   sources = {
     { name = "luasnip" },
     { name = "buffer" },
+    { name = "nvim_lsp"},
     { name = "path" },
+
   },
+  experimental = {
+      ghost_text = true,
+  }
 })
