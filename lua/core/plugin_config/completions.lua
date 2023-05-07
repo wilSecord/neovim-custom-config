2e6a8aad13a27e63fde44ab908e26ebcac6aef2c
wil <wilsonsecord@gmail.com>
2023-05-07 14:48:40 -0400

    Fixed up some completion issures regarding snippets

--- lua/core/plugin_config/completions.lua
+++ lua/core/plugin_config/completions.lua
@@ -1,6 +1,11 @@
 local cmp = require("cmp")
-
+local luasnip = require("luasnip")
 cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end
+  },
   mapping = cmp.mapping.preset.insert({
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
