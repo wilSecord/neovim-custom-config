b150b2f86759dd7c1786990278f6d9d7d8df58b9
wilSecord <wilsonsecord@gmail.com>
2025-03-31 13:33:50 -0400

    trying to get concealer to work on neorg. no such luck so far

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -8,6 +8,7 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
+vim.opt.conceallevel = 3
 
 -- vim.cmd('autocmd BufNewFile *.tex 0r ~/.config/nvim/templates/tex.tex')
 -- vim.cmd('autocmd BufNewFile *.sh 0r ~/.config/nvim/templates/shell.sh')
---
+++ lua/core/plugin_config/neorg.lua
@@ -0,0 +1,12 @@
+require('neorg').setup({
+    load = {
+        ["core.defaults"] = {},
+        ["core.concealer"] = {
+        },
+        ["core.completion"] = {
+            config = {
+              engine = "nvim-cmp",
+            },
+        },
+    }
+})
