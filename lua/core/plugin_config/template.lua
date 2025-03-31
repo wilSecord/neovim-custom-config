f9e9a2192c7b1d4a997da08578b9e6c294dd6a84
wilSecord <wilsonsecord@gmail.com>
2025-03-31 11:31:13 -0400

    Moved some things around.

--- lua/core/plugin_config/template.lua
+++ lua/core/plugin_config/template.lua
@@ -6,9 +6,3 @@ tplt.setup({
 tplt.register('{{_path_}}', function() vim.fn.expand('%') end)
 require('telescope').load_extension('find_template')
 
-vim.api.nvim_create_autocmd("BufNewFile", {
-    callback = function ()
-        pattern = vim.bo.filetype
-        vim.cmd([[:Template pattern]])
-    end
-})
