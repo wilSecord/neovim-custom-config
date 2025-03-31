d5ed4cce413c5b65fb885fdd71d37bd2399085b5
wilSecord <wilsonsecord@gmail.com>
2025-03-31 10:01:20 -0400

    lua/core/plugin_config/knap.lua

--- lua/core/plugin_config/knap.lua
+++ lua/core/plugin_config/knap.lua
@@ -18,7 +18,7 @@ local gknap_settings = {
     textopdfviewerlaunch: "zathura --synctex-editor-command 'nvim --headless -es --cmd \"lua require('\"'\"'knaphelper'\"'\"').relayjump('\"'\"'%servername%'\"'\"','\"'\"'%{input}'\"'\"',%{line},0)\"' %outputfile%",
     textopdfviewerrefresh: "none",
     textopdfforwardjump: "zathura --synctex-forward=%line%:%column%:%srcfile% %outputfile%"
-\ }
+}
 local gknapsettings = {
     texoutputext = "pdf",
     textopdf = "pdflatex -synctex=1 -halt-on-error -interaction=batchmode %docroot%",
