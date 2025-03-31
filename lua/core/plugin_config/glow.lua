ae6c3fc20f8a13b90fc56c30d7faa626c03550ff
wilSecord <wilsonsecord@gmail.com>
2025-03-31 09:57:05 -0400

    lua/core/plugin_config/glow.lua

--- lua/core/plugin_config/glow.lua
+++ lua/core/plugin_config/glow.lua
@@ -2,7 +2,7 @@ require('glow').setup({
     {
     glow_path = "", -- will be filled automatically with your glow bin in $PATH, if any
     install_path = "~/.local/bin", -- default path for installing glow binary
-    border = "", -- floating window border config
+    border = "line", -- floating window border config
     style = "light", -- filled automatically with your current editor background, you can override using glow json style
     pager = false,
     width = 80,
