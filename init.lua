dc5be634e15836e739d17e7668174674f0f61e90
wilSecord <wilsonsecord@gmail.com>
2025-03-31 11:17:48 -0400

    Switched to lazy.nvim instead of Packer.

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
-require('core.plugin_config.lazy')
 require('core.main_theme')
-require('core.plugins')
+require('core.plugin_config.lazy')
 require('core.plugin_config')
+require('plugins')
 require('core.options')
 vim.cmd('NvimTreeToggle')
 
