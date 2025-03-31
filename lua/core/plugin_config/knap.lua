49abe5cf8d7e1263f9973b06297ad10ba7802462
wilSecord <wilsonsecord@gmail.com>
2025-03-31 10:04:36 -0400

    lua/core/plugin_config/knap.lua

--- lua/core/plugin_config/knap.lua
+++ lua/core/plugin_config/knap.lua
@@ -9,7 +9,7 @@ kmap({ 'n', 'v', 'i' },'<F5>', function() require("knap").process_once() end)
 kmap({ 'n', 'v', 'i' },'<F6>', function() require("knap").close_viewer() end)
 
 -- F7 toggles the auto-processing on and off
-kmap({ 'n', 'v', 'i' },'<F7>', function() require("knap").toggle_autopreviewing() end)
+kmap({ 'n', 'v', 'i' },'<F9>', function() require("knap").toggle_autopreviewing() end)
 
 -- F8 invokes a SyncTeX forward search, or similar, where appropriate
 kmap({ 'n', 'v', 'i' },'<F8>', function() require("knap").forward_jump() end)
