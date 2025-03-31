384e66d100261029022b0a44995e858f63c640c0
wilSecord <wilsonsecord@gmail.com>
2025-03-31 11:23:43 -0400

    lua/core/options.lua

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -11,4 +11,4 @@ vim.opt.expandtab = true
 
 -- vim.cmd('autocmd BufNewFile *.tex 0r ~/.config/nvim/templates/tex.tex')
 -- vim.cmd('autocmd BufNewFile *.sh 0r ~/.config/nvim/templates/shell.sh')
-vim.cmd('autocmd BufWritePost * silent! execute "! if [-d .git] || git rev-parse --git-dir > /dev/null 2>&1 ; then git add % ; git commit -m %; fi > /dev/null 2>&1"')
+vim.cmd('autocmd ExitPre * silent! execute "! if [-d .git] || git rev-parse --git-dir > /dev/null 2>&1 ; then git add % ; git commit -m %; fi > /dev/null 2>&1"')
